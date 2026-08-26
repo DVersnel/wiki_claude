cf225e18016852d59ec31736bd5e746e9fda5891
DVersnel <dcaversnel@gmail.com>
2026-08-26 16:30:41 +0200

    commit

--- ArticleReviewApp/ArticleReviewApp/MainWindow.xaml.cs
+++ ArticleReviewApp/ArticleReviewApp/MainWindow.xaml.cs
@@ -20,7 +20,6 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
-        MainWindowViewModel vm = new MainWindowViewModel();
-        DataContext = vm;
+        
     }
 }
---
+++ ArticleReviewApp/ArticleReviewApp/Views/UserControls/MenuBar.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace ArticleReviewApp.Views.UserControls
+{
+    public partial class MenuBar : UserControl
+    {
+        public MenuBar()
+        {
+            InitializeComponent();
+        }
+    }
+}
